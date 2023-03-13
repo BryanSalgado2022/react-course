146da93faa1af5892bff23bd439bbc1ce8730993
BryanSalgadoCode <bsalgado@itstrategys.com>
2023-03-13 11:15:26 -0500

    New files order

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
-import logo from './logo.svg';
 import { useState } from 'react';
-//import './App.css';
 import React from 'react';
-import { Counter } from './Components/CounterComponent';
-import { Search } from './Components/SearchComponent';
-import { List } from './Components/ListComponent';
-import { Item } from './Components/ItemComponent';
-import { AddTodoButton } from './Components/AddTodoButtonComponent';
+import { AppUI } from './Components/pages/AppUI';
 
 
 const defaultTodos = [
@@ -60,54 +54,15 @@ function App(/*props*/) {
     setTodos(newTodos);
   }
   return (
-    /*<div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {
-            //Las props.propiedades están llamando al contenido que viene desde index.js
-            //props.propiedades
-            //para llamar las propiedades children, se hace de la siguiente manera:
-            props.children
-          }
-        </a>
-      </header>
-    </div>*/
-
-    //React fragment renderiza un espacio invisible el cual no afectará la estructura del HTML
-    <>
-      <Counter
-        total={totalTodos}
-        completed={completedTodos}
-      />
-      <Search
-        searchValue={search}
-        setSearch={setSearch}
-      />
-      <List>
-        {
-          searchedTodos.map((todo) => (
-            <Item 
-              key={todo.text} 
-              text={todo.text}
-              completed={todo.completed}
-              onComplete={() => completeTodo(todo.text)}
-              onDelete={() => deleteTodo(todo.text)}
-            />
-          ))
-        }
-      </List>
-      <AddTodoButton/>
-      
-    </>
+    <AppUI
+      total={totalTodos}
+      completed={completedTodos}
+      searchValue={search}
+      setSearch={setSearch}
+      searchedTodos={searchedTodos}
+      completeTodo={completeTodo}
+      deleteTodo={deleteTodo}
+    />
   );
 }
 
